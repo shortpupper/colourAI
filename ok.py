d84f56807736ff006113dcea30230af3a14218fd
shortpupper <78228290+shortpupper@users.noreply.github.com>
2022-04-24 21:02:00 -0500

    Update ok.py
    ysh

--- ok.py
+++ ok.py
@@ -3,7 +3,7 @@ import json
 from random import randrange as rng
 
 def load_weights():
- with open(r"E:\Python\prog\how\ai-stouf\waeight-new.json") as f: return json.loads(f.read())
+ with open(r"E:\Python\prog\how\ai-stouf\waeight-new-o.json") as f: return json.loads(f.read())
 
 
 colour_rgb = (255, 43, 0)
@@ -11,7 +11,7 @@ colour_rgb = (255, 43, 0)
 learning_rate = 0.0001 #0.000001
 #bias = 1
 roundToInt = 7
-threshold = 1
+threshold = 5
 whenModNodeHowManyTimes = 1
 
 
@@ -23,13 +23,13 @@ weightsDict.pop("bias")
 weights = [list(weightsDict.items())[i][1:][0] for i in range(len(weightsDict.keys()))]
 
 # constants
-CARV = "+-"
+CARV = "+++-"
 
 # function to save the weights to a json file
 def save_weights(bias, weights):
  data = {"bias": bias}
  for i in range(len(weights)): data[f"hidden{i}"] = weights[i]
- with open(r"E:\Python\prog\how\ai-stouf\waeight-new.json", "w") as f: f.write(str(json.dumps(data)))
+ with open(r"E:\Python\prog\how\ai-stouf\waeight-new-o.json", "w") as f: f.write(str(json.dumps(data)))
 
 # function to get change
 def getChange(current, previous):
@@ -58,17 +58,25 @@ def hiddenlayer(bias:int, inputs:list[float], weights:list[list[float]]) -> list
  return [perceptron(bias, inputs, weights[i]) for i in range(len(weights))]
 
 # function to make a list of random numbers with args as count and round n
-def nodeMaker(count: int, roundTo: int) -> list[int]: return [round(random.random(), roundTo) for i in range(count)]
+def nodeMaker(count: int, roundTo: int) -> list[int]: return [round(rng(0, 3)/10, roundTo) for i in range(count)]
 
 # function to make blank nodes
-def blankNodeMaker(count: int) -> list[int]: return [0.0 for i in range(count)]
+def blankNodeMaker(count: int) -> list[int]: return [0.0]*count
 
 #print(nodeMaker(14, 7))
 # function to make hiddenlayers from nodeMaker
-def makeHiddenLayer(countHiddenLayers: int, nodeCount: int, roundTo: int):
- return [blankNodeMaker(nodeCount) for i in range(countHiddenLayers)]
+def makeHiddenLayer(countHiddenLayers: int, nodeCount: int):
+ return [nodeMaker(nodeCount, 7) for i in range(countHiddenLayers)]
 
+# function to make network nodes
+def makeNetworkNode(count: int, nodeCount: int, countHiddenLayers: int): return [makeHiddenLayer(countHiddenLayers, nodeCount) for i in range(count)]
 
+"""no = makeNetworkNode(10, 10, 10)
+print(no)
+
+save_weights(0, no)
+print("saving")
+"""
 """hiddenlayerA = hiddenlayer(bias, Inputs(colour_rgb), weights[0])
 
 hiddenlayerB = hiddenlayer(bias, hiddenlayerA, weights[1])
@@ -103,6 +111,19 @@ def modifyNode(weights: list[list[list[float]]], times: int) -> list[list[list[f
   else: weights[r1][r2][r3] += learning_rate
  return weights
 
+def testNet(bias, weights, text: bool=False, *args, **kwargs,):
+ GoodColoursRgbList = [(32, 179, 152), (41, 189, 8), (255, 208, 0)]
+ BadColoursRgbList =  [(214, 17, 211), (0, 4, 240), (255, 0, 140)]
+ if text and type(text) == bool:
+  for x in GoodColoursRgbList:
+   c = network(bias, weights, x)
+   print(f"good colour think:{c}")
+   print("I think you like this colour") if c >= threshold  else print("I think you don't like this colour")
+  for x in BadColoursRgbList:
+   c = network(bias, weights, x)
+   print(f"Bad colour think:{c}")
+   print("I think you like this colour") if c >= threshold  else print("I think you don't like this colour")
+ else: return (sum([network(bias, weights, x)-threshold for x in GoodColoursRgbList])/len(GoodColoursRgbList)+sum([network(bias, weights, x)-threshold for x in BadColoursRgbList])/len(BadColoursRgbList))/2
 
 
 # function to test the network for errors
@@ -111,17 +132,7 @@ def errorCatcher(bias: int, weights: list, real: list[tuple[int]], fake: list[tu
 
 
 # function to for modifly the network weights and or bias
-def networkMod(bias: float, weights: list) -> tuple[float, list]:
- what = rng(0, 3)
- # if what is 0
- if not what: weights = modifyNode(weights, whenModNodeHowManyTimes)
- # if what is 1
- elif what-1: bias = round(bias-float(CARV[rng(0,2)]+str(learning_rate)), roundToInt)
- # if what is 2
- else:
-  weights = modifyNode(weights, whenModNodeHowManyTimes)
-  bias = round(bias-float(CARV[rng(0,2)]+str(learning_rate)), roundToInt)
- return (bias, weights)
+def networkMod(bias: float, weights: list) -> tuple[float, list]: return (round(bias+float(CARV[rng(0,4)]+str(learning_rate)), roundToInt), modifyNode(weights, whenModNodeHowManyTimes))
  
 
 
@@ -130,22 +141,26 @@ BadColoursRgbList =  [(214, 17, 211), (0, 4, 240), (255, 0, 140)]
 
 # function to train the network
 def train(count: int):
- printEvery = 1_000
+ printEvery = 200
  bestWeight, bestBias = weights, bias
  for i in range(count):
   print(f"round {i}") if i % printEvery == 0 else None  
   net1Mod = networkMod(bestBias, bestWeight)
-  net1 = errorCatcher(net1Mod[0], net1Mod[1], GoodColoursRgbList, BadColoursRgbList)
+  net1 = testNet(net1Mod[0], net1Mod[1], GoodColoursRgbList, BadColoursRgbList)
   net2Mod = networkMod(bestBias, bestWeight)
-  net2 = errorCatcher(net2Mod[0], net2Mod[1], GoodColoursRgbList, BadColoursRgbList)
+  net2 = testNet(net2Mod[0], net2Mod[1], GoodColoursRgbList, BadColoursRgbList)
   net3Mod = networkMod(bestBias, bestWeight)
-  net3 = errorCatcher(net3Mod[0], net3Mod[1], GoodColoursRgbList, BadColoursRgbList)
+  net3 = testNet(net3Mod[0], net3Mod[1], GoodColoursRgbList, BadColoursRgbList)
   net4Mod = networkMod(bestBias, bestWeight)
-  net4 = errorCatcher(net4Mod[0], net4Mod[1], GoodColoursRgbList, BadColoursRgbList)
+  net4 = testNet(net4Mod[0], net4Mod[1], GoodColoursRgbList, BadColoursRgbList)
   net5Mod = networkMod(bestBias, bestWeight)
-  net5 = errorCatcher(net5Mod[0], net5Mod[1], GoodColoursRgbList, BadColoursRgbList)
+  net5 = testNet(net5Mod[0], net5Mod[1], GoodColoursRgbList, BadColoursRgbList)
   nets = [net1, net2, net3, net4, net5]
-  j = nets.index(max(nets))
+  nets2 = nets
+  nets2.sort()
+  nets2 = [x for x in nets2 if x >= 0]
+  j = nets.index(nets2[0])
+  #j = nets.index(max(nets))
   bestBias, bestWeight = [net1Mod, net2Mod, net3Mod, net4Mod, net5Mod][j]
   print(nets) if i % printEvery == 0 else None
   print(f"best network is {j}") if i % printEvery == 0 else None
@@ -154,7 +169,7 @@ def train(count: int):
 
 
 
-#train(2_000)
+train(1_000)
 
 # function to tst a
 def test():
@@ -166,20 +181,9 @@ def test():
  print(net1Mod)
  print(net1)
 
-def testNet():
- GoodColoursRgbList = [(32, 179, 152), (41, 189, 8), (255, 208, 0)]
- BadColoursRgbList =  [(214, 17, 211), (0, 4, 240), (255, 0, 140)]
- for x in GoodColoursRgbList:
-  c = network(bias, weights, x)
-  print(f"good colour think:{c}")
-  print("I think you like this colour") if c >= threshold  else print("I think you don't like this colour")
- for x in BadColoursRgbList:
-  c = network(bias, weights, x)
-  print(f"Bad colour think:{c}")
-  print("I think you like this colour") if c >= threshold  else print("I think you don't like this colour")
- 
 
-#testNet()
+testNet(bias, weights, True)
+
 """
 #what it thinks
 ansewr = network(bias, weights, (255, 0, 140))
@@ -197,3 +201,21 @@ else: print("I think you don't like this colour")
 # ideas
 
 # maby when the input is RGB in stead mkae it so it a per sent like 0.4 of the normal colour value in stead of 0 or 1
+
+
+"""
+
+
+----\
+-----\
+------*
+
+-----\
+------*
+-----/
+
+------*
+-----/
+----/
+
+"""
